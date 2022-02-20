9f81b4d0d47801578559354ecd21becb137c8bd2
mishanka122 <mishanka.99@yandex.ru>
2022-02-20 20:17:16 +0300

    Create MainPage. test topics

--- log_env/Scripts/log/logs/views.py
+++ log_env/Scripts/log/logs/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Topic
+ 
+def index(request):
+	topics = Topic.objects.order_by('-date_added')
+	context = {
+		'topics': topics,
+	}
+	return render(request,
+		'logs/index.html', context)
+ 
+def topic(request, topic_id):
+	topic = Topic.objects.get(id=topic_id)
+	entries = topic.entry_set.order_by('-date_added')
+	context = {
+		'topic': topic,
+		'entries': entries,
+	}
+	return render(request,
+		'logs/topic.html', context)
